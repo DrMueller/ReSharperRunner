dede6afcda51b86bf61a3f84ea97d00994ec25f3
Müller Matthias <matthias.mueller@noser.com>
2024-08-19 11:56:43 +0200

    WIP

--- ReSharperRunnerTask/analysis/services/analysis-runner.ts
+++ ReSharperRunnerTask/analysis/services/analysis-runner.ts
@@ -1,4 +1,3 @@
-import { ChildProcess } from 'child_process';
 const { exec } = require('child-process-async');
 import { AnalysisResult } from '../models/analysis-result';
 import { readFileSync } from 'fs';
@@ -20,8 +19,14 @@ export class AnalysisRunner {
         }
 
         // Maxbuffer seems required on certain machines, see https://stackoverflow.com/questions/23429499/stdout-buffer-issue-using-node-child-process
-        await exec(command, {maxBuffer: 1024 * 500});
+        const { stdout, stderr } = await exec(command, { maxBuffer: 1024 * 500 });
 
+        console.log('Standard Output:', stdout);
+
+        if (stderr) {
+            console.error('Error Output:', stderr);
+        }
+        
         if (!fs.existsSync(outputFile)) {
             return new AnalysisResult(null);
         }
