8903672740a3796366253b782fdab2ad6a558fe8
Müller Matthias <matthias.mueller@noser.com>
2024-09-10 13:13:08 +0200

    WIP

--- ReSharperRunnerTask/analysis/services/analysis-runner.ts
+++ ReSharperRunnerTask/analysis/services/analysis-runner.ts
@@ -1,21 +1,24 @@
 import { spawn } from 'child_process';
 import { AnalysisResult } from '../models/analysis-result';
 import { existsSync, unlinkSync, readFileSync } from 'fs';
+import tl = require('azure-pipelines-task-lib/task');
 
 export class AnalysisRunner {
     public async runAnalysisAsync(
         solutionPath: string,
         additionalArguments: string | undefined): Promise<AnalysisResult> {
 
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             const outputFile = 'output.json';
-          
+
             // Wenn die Datei bereits existiert, löschen
             if (existsSync(outputFile)) {
                 unlinkSync(outputFile);
             }
 
-            const process = spawn('jb', ['inspectcode', solutionPath, `-o=${outputFile}`, ...(additionalArguments ? [additionalArguments] : [])]);
+            const argsArray = ['inspectcode', solutionPath, `-o=${outputFile}`, additionalArguments ?? ''];
+            const process = spawn('jb', argsArray);
+            tl.debug(argsArray.join(', '));
 
             process.stdout.on('data', (data) => {
                 console.log(`${data}`);
